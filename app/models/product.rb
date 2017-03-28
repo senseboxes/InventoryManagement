902335e97cff01ea87276a2a02d148c9414b118a
LB.SEOK <sense2pro@gmail.com>
2017-03-28 15:47:27 +0900

    소스정리 및 버튼크기조정

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,26 +1,6 @@
 class Product < ApplicationRecord
-#  attr_accessor :puchase_kg, :release_kg, :stock_kg, :predict, :memo
-
-  #validates_presence_of :stock_kg
-  #validates :stock_kg, numericality: true  #stock_kg의 값은 반드시 존재해야함
   belongs_to :inventory
-  self.per_page = 10 # 1페이지에 데이터를 10개만 보여줘 : 넘으면 다음 페이지를 생성
 
-#  def self.to_csv(options = {})
-#    CSV.generate(options) do |csv|
-#      csv << column_names
-#      all.each do |product|
-#      csv << product.attributes.values
-#      end
-#    end
-#  end
-
-# Export 할 때 이와 같은 구조로 출력이 됨
-# csv desired_columns 먼저 output
-# product의 값이 desired_columns에 맞게 전부 output
-#    ====결과====
-#  컬럼1  컬럼3   컬럼2 ...
-# 데이터1 데이터3 데이터2 ...
   def self.to_csv(options = {})
     desired_columns = ["id", "pname", "puchase_kg", "release_kg", "stock_kg", "predict", "month_avg", "memo", "inventory_id", "created_at", "updated_at"]
     CSV.generate(options) do |csv|
--- app/models/product_import.rb
+++ app/models/product_import.rb
@@ -14,9 +14,7 @@ class ProductImport
     false
   end
 
-# ProductImportsController의 create에서 보냄
   def save
-#   imported_products의 유효성 검사를 함
     if imported_products.map(&:valid?).all?
       imported_products.each(&:save!)
       true
@@ -31,24 +29,17 @@ class ProductImport
   end
 
   def imported_products
-# => @imported_products는 load_imported_products와 동일
     @imported_products ||= load_imported_products
   end
 
   def load_imported_products
-# => 스프레드 시트를 작성한다. ROO라는 rails gem으로 작성
+
     spreadsheet = Roo::Spreadsheet.open(file.path)
-# => 헤더(id, pname, puchase_kg같은 필드 네임)는 row 1번째 줄이다.
     header = spreadsheet.row(1)
-# => 2번째 줄부터 마지막까지 mapping??
     (2..spreadsheet.last_row).map do |i|
-# => row(1) = header, row(2) = 첫번 째 데이터 ...... i만큼의 row가 생성
       row = Hash[[header, spreadsheet.row(i)].transpose]
-# => product는 Product.new와 동일 (고로 nil값을 가진 필드네임 : id:nil, pname:nil...)
       product = Product.find_by(id: row["id"]) || Product.new
-# => row.to_hash값을 product.attributes로 저장
       product.attributes = row.to_hash
-# => 최종적인 값은 product = load_imported_products
 
       @ProductsController = ProductsController.new
       @ProductsController.import_create(product)
